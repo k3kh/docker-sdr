7b4f82e3c7242ed06d942e9a3bb35856aad1f2cb
Chris Beer <cabeer@stanford.edu>
2017-09-28 11:32:10 -0700

    Add script for running db:create and db:migrate for applications that use databases

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker-compose run suri rake db:create db:migrate
+docker-compose run argo rake db:create db:migrate
+docker-compose run hydrus rake db:create db:migrate
+docker-compose run purl-fetcher rake db:create db:migrate
+docker-compose run discovery-dispatcher rake db:create db:migrate
+docker-compose run workflow-service rake db:create db:migrate
