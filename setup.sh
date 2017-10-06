beee9b1c6bd4b2117b98314ddd47a727c4655538
Chris Beer <chris@cbeer.info>
2017-10-06 09:10:15 -0700

    Argo db migration only need to be run once

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 docker-compose run suri rake db:create db:migrate
 docker-compose run argo-web rake db:create db:migrate
-docker-compose run argo-workers rake db:create db:migrate
 docker-compose run hydrus rake db:create db:migrate
 docker-compose run purl-fetcher rake db:create db:migrate
 docker-compose run discovery-dispatcher rake db:create db:migrate
